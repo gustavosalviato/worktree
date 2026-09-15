1020d396279fa2cdde7b0cba6f8ab6502cec43ce
gustavosalviato <gustavosalviato8@gmail.com>
2026-09-15 19:44:48 -0300

    cleanup: remove unused tests

--- tests/WebAPI.Test/User/ChangePassword/ChangePasswordTests.cs
+++ tests/WebAPI.Test/User/ChangePassword/ChangePasswordTests.cs
@@ -1,12 +1,7 @@
-using System.Globalization;
 using System.Net;
-using System.Text.Json;
 using CommonTestUtilities.Requests;
 using Shouldly;
-using WebAPI.Test.InlineData;
 using WebAPI.Test.Resources;
-using WorkTree.Communication.Requests.Users;
-using WorkTree.Exceptions;
 
 namespace WebAPI.Test.User.ChangePassword;
 
@@ -32,35 +27,4 @@ public class ChangePasswordTests : BaseIntegrationTest
 
         response.StatusCode.ShouldBe(HttpStatusCode.NoContent);
     }
-
-    // [Theory]
-    // [ClassData(typeof(CultureInlineData))]
-    // public async Task Validate_ShouldThrowException_WhenNewPasswordIsEmpty(string culture)
-    // {
-    //     var request = new RequestChangePasswordJson
-    //     {
-    //         NewPassword = string.Empty,
-    //         CurrentPassword = _firstUser.GetPassword()
-    //     };
-    //
-    //     var response = await Put(RequestUri, request, _firstUser.GetAccessToken(), culture);
-    //
-    //     response.StatusCode.ShouldBe(HttpStatusCode.BadRequest);
-    //
-    //     await using var responseBody = await response.Content.ReadAsStreamAsync();
-    //     
-    //     var responseData = await JsonDocument.ParseAsync(responseBody);
-    //     var errors = responseData.RootElement.GetProperty("errors").EnumerateArray();
-    //     
-    //     var expectedErrorMessage =
-    //         ResourceMessagesException.ResourceManager.GetString("VALIDATION_PASSWORD_REQUIRED",
-    //             new CultureInfo(culture));
-    //     
-    //     errors.ShouldSatisfyAllConditions(errorsList =>
-    //     {
-    //         errorsList.Count().ShouldBe(1);
-    //         errorsList.ShouldContain(error =>
-    //             error.GetString()!.Equals(expectedErrorMessage));
-    //     });
-    // }
 }
